c4c0c37438272caaa60958cf721f512e8d63980e
Alexander <baratashvilialex@gmail.com>
2025-06-25 22:36:40 +0400

    finishedwith the backend just have to connect the server to a mongo db

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,3 +24,57 @@ export const createUser = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 }
+
+// @desc get all users
+// @route GET /users
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        if (!users || users.length === 0) {
+            return res.status(404).json({ message: "No users found" });
+        }
+        res.status(200).json(users);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+// @desc update a user
+// @route PUT /users/:id
+export const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, email, address } = req.body;
+        const user = await User.findByIdAndUpdate(id,
+            { name, email, address },
+            { new: true, runValidators: true }
+        )
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User updated successfully", user });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "error updating the user" });
+    }
+}
+
+// @desc delete a user
+// @route DELETE /users/:id
+export const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndDelete(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User deleted successfully" });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "error deleting the user" });
+    }
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,23 @@
 import express from 'express';
-import { createUser } from '../controllers/userController.js';
+import { createUser,
+         getUsers,
+         updateUser,
+         deleteUser}
+  from '../controllers/userController.js';
 
 const router = express.Router();
 
 // Route to create a user
 router.post("/", createUser);
+
+// Route to get all users
+router.get("/", getUsers);
+
+// Route to update a user by ID
+router.put("/:id", updateUser);
+
+// Route to delete a user by ID
+router.delete("/:id", deleteUser);
 
 // Export the router
 export default router;
